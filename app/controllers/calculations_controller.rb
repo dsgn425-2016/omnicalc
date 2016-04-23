50f0254b02838e2ce41885691af2819227b28e99
RamyaKellogg <rsethurathinam2017@kellogg.northwestern.edu>
2016-04-23 18:28:41 -0500

    Word Count

--- app/controllers/calculations_controller.rb
+++ app/controllers/calculations_controller.rb
@@ -10,14 +10,15 @@ class CalculationsController < ApplicationController
     # The special word the user input is in the string @special_word.
     # ================================================================================
 
+    textarray = @text.split
 
-    @character_count_with_spaces = "Replace this string with your answer."
+    @character_count_with_spaces = @text.length
 
-    @character_count_without_spaces = "Replace this string with your answer."
+    @word_count = @text.split.size
 
-    @word_count = "Replace this string with your answer."
+    @character_count_without_spaces = @character_count_with_spaces-@word_count+1
 
-    @occurrences = "Replace this string with your answer."
+    @occurrences = textarray.count(@special_word)
 
     # ================================================================================
     # Your code goes above.
