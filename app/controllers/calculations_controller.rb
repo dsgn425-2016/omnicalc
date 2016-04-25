dc4b3244ae170dc8d01f9c6226454ee4cd500d31
adamchutch <adamchutch@users.noreply.github.com>
2016-04-25 14:25:25 -0500

    Monthly Loan

--- app/controllers/calculations_controller.rb
+++ app/controllers/calculations_controller.rb
@@ -37,7 +37,7 @@ class CalculationsController < ApplicationController
     # The principal value the user input is in the decimal @principal.
     # ================================================================================
 
-    @monthly_payment = "Replace this string with your answer."
+    @monthly_payment = ((@apr/12)*@principal)/(1-(1+(@apr/12)-(@years*12)))
 
     # ================================================================================
     # Your code goes above.
